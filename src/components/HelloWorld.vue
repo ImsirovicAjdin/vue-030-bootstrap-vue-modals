2a46de28459957f2feb32671c714adbfdeebff97
FlowOverstack <ajdika@live.com>
2020-10-23 09:44:40 +0200

    Import and use BootstrapVue's Button and Modal component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import { BButton, BModal } from "bootstrap-vue";
+
 export default {
   name: "HelloWorld",
+  components: {
+    BButton,
+    BModal,
+  },
   props: {
     msg: String,
   },
